e8b44d933130938fc5b6e998a47b548561fb70fc
YutaGoto <you.goto.510@gmail.com>
2020-08-25 23:42:34 +0900

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import liff from '@line/liff';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const sendMessage = () => {
+    liff.init({liffId: process.env.LIFF_ID as string})
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({})
+        }
+        liff.sendMessages([{
+            'type': 'text',
+            'text': "You've successfully sent a message! Hooray!"
+        }]).then(function() {
+            window.alert('Message sent');
+        }).catch(function(error) {
+            window.alert('Error sending message: ' + error);
+        });
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +28,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button className="button" onClick={sendMessage}>button</button>
         <a
           className="App-link"
           href="https://reactjs.org"
