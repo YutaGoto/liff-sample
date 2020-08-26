23617d5216b33ad519cb59736a33f57bdd3b9ce9
YutaGoto <you.goto.510@gmail.com>
2020-08-26 16:01:37 +0900

    add get user info

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,25 @@ function App() {
       })
   }
 
+  const getUserInfo = () => {
+    liff.init({liffId: process.env.REACT_APP_LIFF_ID as string})
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({})
+        } else if (liff.isInClient()) {
+          liff.getProfile()
+            .then(profile => {
+              const userId: string = profile.userId
+              const displayName: string = profile.displayName
+              alert(`Nane: ${displayName}, userId: ${userId}`)
+            }).catch(function(error) {
+              window.alert('Error sending message: ' + error);
+            });
+        }
+      })
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -30,6 +49,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <button className="button" onClick={sendMessage}>send message</button>
+        <button className="button" onClick={getUserInfo}>show user info</button>
         <a
           className="App-link"
           href="https://reactjs.org"
